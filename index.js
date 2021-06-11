5f0fa9a1d7f6bb3a20fa8fa972f4d5b0f971db62
tim <tim@twa.ninja>
2021-06-10 21:20:40 -0600

    Added blog post component and routing.

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
 Vue.use(VueRouter);
-/*
-How to load content from content.json:
-<div
-	v-if="is_content_loading"
->loading...</div>
-<div
-	v-else
-	class="blog-list"
->
-	<div
-		class="blog-item"
-		v-for="blog_post of content"
-	:key="blog_post.id"
->
-	<a
-	:href="blog_post.id"
->{{ blog_post.title }}</a>
-</div>
-</div>
-*/
+
 const props_for_root_router_view = {
 	content: {
 		type: Array,
@@ -46,14 +27,54 @@ const Home = {
 					v-for="blog_post of content"
 					:key="blog_post.id"
 				>
-					<a
-					:href="blog_post.id"
-				>{{ blog_post.title }}</a>
+					<router-link
+						:to="'/blog/' + blog_post.id"
+					>{{ blog_post.title }}</router-link>
 				</div>
 			</div>
 		</div>
 	`
 };
+
+const BlogPost = {
+	props: {
+		...props_for_root_router_view, // 'Spread' Operator
+		id: {
+			type: String,
+			required: true,
+		},
+	},
+	computed: {
+		post(){
+			const id = this.id;
+			const is_content_loading = this.is_content_loading;
+			const result = is_content_loading
+				? {}
+				: this.content.find(function(blog_post){
+					return blog_post.id === id;
+				});
+			return result;
+		}
+	},
+	template: `
+		<div 
+			class="blog-post"
+		>
+			<div
+				v-if="is_content_loading"
+			>loading...</div>
+			<div
+				v-else
+				class="blog-post-content"
+			>
+				<h1>{{ post.title }}</h1>
+				<div
+					v-html="post.body"
+				></div>
+			</div>
+		</div>
+	`
+}
 
 const Games = {
 	props: props_for_root_router_view,
@@ -107,6 +128,7 @@ const routes = [
 	{ path: '/sbemails', component: Sbemails },
 	{ path: '/yodeling', component: Yodeling },
 	{ path: '/yodeling2', component: Yodeling2 },
+	{ path: '/blog/:id', component: BlogPost, props: true },
 ];
 
 const router = new VueRouter({
@@ -144,12 +166,12 @@ let app = new Vue({
 	el: '#app',
 	data: {
 		menu_list: [
-			{id: 'home', label: 'Home'},
-			{id: 'games', label: 'Games'},
-			{id: 'toons', label: 'Toons'},
-			{id: 'sbemails', label: 'sbemails'},
-			{id: 'yodeling', label: 'Yodeling'},
-			{id: 'yodeling2', label: 'Yodeling'},
+			{id: '/home', label: 'Home'},
+			{id: '/games', label: 'Games'},
+			{id: '/toons', label: 'Toons'},
+			{id: '/sbemails', label: 'sbemails'},
+			{id: '/yodeling', label: 'Yodeling'},
+			{id: '/yodeling2', label: 'Yodeling'},
 		],
 		is_content_loading: true,
 		content: [],

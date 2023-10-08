65a03d89b46b9a24b210a4f77a9becaea4f4978d
tim <tim@twa.ninja>
2023-10-08 15:43:17 -0600

    reworked how docker is installed

--- server_config/ansible/files/init-letsencrypt.sh
+++ server_config/ansible/files/init-letsencrypt.sh
@@ -7,9 +7,9 @@ fi
 
 domains=(twa.ninja www.twa.ninja)
 rsa_key_size=4096
-data_path="/home/tim/twa.ninja/server_config/data/certbot/"
+data_path="/root/twa.ninja/server_config/ansible/files/data/certbot/"
 email="tim@twa.ninja" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
